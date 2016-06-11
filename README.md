01e8196a6abff9d5d582e48f922809b1b318689e
Nick P Smith <npsmith1990@gmail.com>
2016-06-11 08:21:25 -0600

    Create a React Starter

---
+++ README.md
@@ -0,0 +1,12 @@
+# React Starter
+
+#### Getting Started
+
+```
+  > git clone git@github.com:nicholaspsmith/BeerRecipes.git
+  > cd BeerRecipes
+  > npm install
+  > npm start
+```
+
+View the app by navigating to [localhost:8080](http://localhost:8080)
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        To get started, edit /src/index.js
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('ReactApp'))
